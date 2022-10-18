348b626a97605a6f2318b784338ae501a6df8c24
Fuzzycast <ronincampbell3@gmail.com>
2022-10-18 12:53:52 +1100

    Level 2 Progress

---
+++ Assets/Scripts/Wallrun_PickUp.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wallrun_PickUp : MonoBehaviour
+{
+    public GameObject player;
+
+    void OnTriggerEnter(Collider other)
+    {
+        player.GetComponent<WallRunning>().enabled = true;
+        Debug.Log(player);
+        Destroy(gameObject);
+    }
+}
